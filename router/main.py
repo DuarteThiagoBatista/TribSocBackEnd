c93604188094d7bea64407adf11d9fe6b6e41eb5
Paulo Daniel <paulo.daniel@souunit.com.br>
2022-06-03 22:33:29 -0300

    mudança main

--- router/main.py
+++ router/main.py
@@ -45,20 +45,4 @@ def login(login_data:LoginData,database:Session=Depends(get_db)):
 @app.get('/converter/{dolar}')
 def main(dolar:float):
     return converterDolarToReal(dolar)
-    '''moeda= requests.get("https://economia.awesomeapi.com.br/last/USD-BRL")
-    cotacao_dolar=round(float(moeda.json()["USDBRL"]["ask"]),2)
-    converter=cotacao_dolar*dolar
-    valor_IRRF=round(calculo_irrf(converter),2)
-    return {'valor_inserido':dolar,"dolar":cotacao_dolar,"convertido":"%.2f" % converter,"IRRF":valor_IRRF,"salario_liquido":"%.2f" %(converter-valor_IRRF)}
-'''
-def calculo_irrf(real):
-    if real<= 1903.98:
-        return 0
-    elif real<=2826.65:
-        return (real*7.5)/100
-    elif real<=3751.05:
-        return (real*15)/100
-    elif real<=4664.68:
-        return (real*22.5)/100
-    else:
-        return(real*27.5)/100
+    
